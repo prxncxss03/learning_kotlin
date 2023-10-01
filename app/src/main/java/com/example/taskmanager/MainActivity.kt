bc04ba5fc4c5491bc2f10063051a68bbad5ab8b9
Princess Pocon <pocon.evangeline@gmail.com>
2023-10-02 01:44:28 +0800

    done task manager

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DoneTaskManagerScreen(
+                        description = stringResource(R.string.all_tasks_completed_text),
+                        comment = stringResource(R.string.nice_work_text)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun DoneTaskManagerScreen(description: String, comment: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+
+
+    )
+    {
+
+        Image (
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = description,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(
+                    start = 0.dp,
+                    top = 24.dp,
+                    bottom = 8.dp,
+                    end = 0.dp
+
+                )
+                .align(alignment = Alignment.CenterHorizontally)
+
+
+        )
+
+        Text (
+            text = comment,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DoneTaskManagerScreen(
+        description = stringResource(R.string.all_tasks_completed_text),
+        comment = stringResource(R.string.nice_work_text)
+    )
+
+}
